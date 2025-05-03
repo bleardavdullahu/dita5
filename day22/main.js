7edba0c30642ed3d0b27f7f946c303689af6c005
bleardavdullahu <bleardavdullahu7@gmail.com>
2025-05-03 05:46:45 -0700

    z

---
+++ day22/main.js
@@ -0,0 +1,25 @@
+document.write("Hello World!");
+
+var x = 10;
+var name ="Ariana"
+
+document.write(x);
+console.log(name);
+
+var a = 50 + 50;
+console.log(a);
+
+var y = 50;
+var z = 100;
+var yz = y = z;
+
+console.log(yz);
+
+
+var n = 99;
+n++;
+console.log(n);
+
+var o = 50;
+o--;
+console.log(o)
