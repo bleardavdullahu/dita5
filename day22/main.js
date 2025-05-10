2cc8dc745f679d3458dab11c8b4c5c0d350dd345
bleardavdullahu <bleardavdullahu7@gmail.com>
2025-05-10 04:29:56 -0700

    ..

--- day22/main.js
+++ day22/main.js
@@ -23,3 +23,20 @@ console.log(n);
 var o = 50;
 o--;
 console.log(o)
+
+var firstName= "Digital";
+var lastName = "School";
+
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+
+var test = "Test123";
+console.log(`Hello from ${test}`);
+
+
+var age = 22;
+var age2 = 24;
+console.log(age >= 18 && age<=65); // and operator
+console.log(age >= 28 || age <=65); // or operator
+console.log(!(age == age2));
